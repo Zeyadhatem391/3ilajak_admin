9b97017cdaa622185e6faaff6dbba61c0c8db796
Zeyadhatem391 <zeyadhatemsabry@gmail.com>
2026-07-20 03:56:52 +0300

    feat(clinics): add ui clinic page

---
+++ src/features/clinics/components/TableClinics.tsx
@@ -0,0 +1,29 @@
+"use client";
+import DataTable from "@/shared/components/data-table/DataTable";
+import DataTablePagination from "@/shared/components/data-table/DataTablePagination";
+import { Search } from "lucide-react";
+import { clinics, clinicsColumns } from "../data/clinic";
+import { Input } from "@/components/ui/input";
+
+function TableClinics() {
+  return (
+    <div className="w-full">
+      <div className="bg-white p-6 shadow flex justify-between items-center border border-gray-300 rounded-t-xl">
+        <h1 className="font-bold text-xl">Detailed Directory</h1>
+        <div className="relative w-full max-w-100">
+          <Search className="absolute top-1/2 left-3 h-5 w-4 -translate-y-1/2 text-gray-900" />
+
+          <Input
+            placeholder="Search Clinic by name"
+            className="pl-10 bg-gray-50 border-gray-200 focus-visible:ring-1"
+          />
+        </div>
+      </div>
+      <DataTable columns={clinicsColumns} data={clinics} />
+
+      <DataTablePagination page={1} totalPages={10} />
+    </div>
+  );
+}
+
+export default TableClinics;
