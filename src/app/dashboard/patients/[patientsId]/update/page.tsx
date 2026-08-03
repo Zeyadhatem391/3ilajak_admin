dd0549deb4c434b6dab3078f981d309412db009d
Zeyadhatem391 <zeyadhatemsabry@gmail.com>
2026-08-03 19:39:20 +0300

    update patient

--- src/app/dashboard/patients/[patientsId]/update/page.tsx
+++ src/app/dashboard/patients/[patientsId]/update/page.tsx
@@ -6,7 +6,7 @@ import FormPatient from "@/features/patients/components/FormPatient";
 function page() {
   return (
     <div className="flex flex-col gap-6">
-      <div className="grid grid-cols-12 items-start gap-6">
+      {/* <div className="grid grid-cols-12 items-start gap-6">
         <div className="col-span-4 flex flex-col gap-6">
           <PhotePatient />
           <StatusPatient />
@@ -14,7 +14,7 @@ function page() {
         <div className="col-span-8 flex flex-col gap-6">
           <FormPatient />
         </div>
-      </div>
+      </div> */}
 
       <div className="flex gap-2 justify-end border-t-2 border-gray-400 pt-6">
         <Button className="h-11 w-24 text-xg text-gray-600 font-semibold cursor-pointer rounded-lg border border-gray-400">
